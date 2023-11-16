082838a5e2cce01383c5b15dae23256aaa99290c
Sica <jessicahu5749@gmail.com>
2023-11-16 14:41:52 -0800

    freeze model ~93% validation acc

--- model.py
+++ model.py
@@ -15,24 +15,31 @@ class DigitClassifierCNN(nn.Module):
             v2.Grayscale(),
         ])
 
-        self.conv1 = nn.Conv2d(1, 6, 3, padding='same')
-        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(576, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(1, 16, 5)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.bn2 = nn.BatchNorm2d(32)
 
-        self.dropout = nn.Dropout(.3)
+        self.fc1 = nn.Linear(512, 128)
+        self.bn3 = nn.BatchNorm1d(128)
+        self.fc2 = nn.Linear(128, 32)
+        self.bn4 = nn.BatchNorm1d(32)
+        self.fc3 = nn.Linear(32, 10)
+
+        self.dropout = nn.Dropout(.5)
 
     def forward(self, x):
         x = self.transform(x)
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+
         x = torch.flatten(x, 1)
         x = self.dropout(x)
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn3(self.fc1(x)))
         x = self.dropout(x)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn4(self.fc2(x)))
         x = self.fc3(x)
 
         return x
